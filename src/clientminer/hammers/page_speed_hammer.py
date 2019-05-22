78f7b845e224fb8cdbf2f71cec0079bcbc71656f
Jeff Magill <jeff@unhingedweb.com>
2019-05-22 10:29:48 -0400

    changes to rendered page speed metrics

--- src/clientminer/hammers/page_speed_hammer.py
+++ src/clientminer/hammers/page_speed_hammer.py
@@ -24,30 +24,26 @@ class PageSpeedHammer:
         # print(data["lighthouseResult"]["audits"]["time-to-first-byte"]["details"]["overallSavingsMs"])
         # print("property:")
         # print(PageSpeedHammer.property("lighthouseResult.audits.time-to-first-byte.details.overallSavingsMs", data))
+        metrics = PageSpeedHammer.property("lighthouseResult.audit.metrics.details.items", data)[0]
         result = {
             "performance-score": PageSpeedHammer.property("lighthouseResult.categories.performance.score", data),
-            "performance-score": PageSpeedHammer.property("lighthouseResult.categories.performance.score", data),
-            "performance-score": PageSpeedHammer.property("lighthouseResult.categories.performance.score", data),
-            "performance-score": PageSpeedHammer.property("lighthouseResult.categories.performance.score", data),
-            "performance-score": PageSpeedHammer.property("lighthouseResult.categories.performance.score", data),
-            "performance-score": PageSpeedHammer.property("lighthouseResult.categories.performance.score", data),
-            "performance-score": PageSpeedHammer.property("lighthouseResult.categories.performance.score", data),
-            "performance-score": PageSpeedHammer.property("lighthouseResult.categories.performance.score", data),
-
-
-            "time-to-first-byte-score": PageSpeedHammer.property("lighthouseResult.audits.time-to-first-byte.score", data),
-            "time-to-first-byte-potential-savings": PageSpeedHammer.property("lighthouseResult.audits.time-to-first-byte.details.overallSavingsMs", data),
-            "render-blocking-resources-score": PageSpeedHammer.property("lighthouseResult.audits.render-blocking-resources.score", data),
-            "render-blocking-resources-potential-savings": PageSpeedHammer.property("lighthouseResult.audits.render-blocking-resources.details.overallSavingsMs", data),
+            "speedIndex": metrics["speedIndex"],
+            "firstContentfulPaint": metrics["firstContentfulPaint"],
+            "firstCPUIdle": metrics["firstCPUIdle",]
+            "firstMeaningfulPaint": metrics["firstMeaningfulPaint"],
             "uses-optimized-images-score": PageSpeedHammer.property("lighthouseResult.audits.uses-optimized-images.score", data),
             "uses-optimized-images-potential-savings": PageSpeedHammer.property("lighthouseResult.audits.uses-optimized-images.details.overallSavingsMs", data),
-            "uses-text-compression-score": PageSpeedHammer.property("lighthouseResult.audits.uses-text-compression.score", data),
-            "uses-text-compression-potential-savings": PageSpeedHammer.property("lighthouseResult.audits.uses-text-compression.details.overallSavingsMs", data),
             "interactive-score": PageSpeedHammer.property("lighthouseResult.audits.interactive.score", data),
             "unminified-css-score": PageSpeedHammer.property("lighthouseResult.audits.unminified-css.score", data),
             "unminified-css-potential-savings": PageSpeedHammer.property("lighthouseResult.audits.unminified-css.details.overallSavingsMs", data),
-            "bootup-time-score": PageSpeedHammer.property("lighthouseResult.audits.bootup-time.score", data),
-            "bootup-time-wasted-time": PageSpeedHammer.property("lighthouseResult.audits.bootup-time.details.summary.wastedMs", data),
+            # "time-to-first-byte-score": PageSpeedHammer.property("lighthouseResult.audits.time-to-first-byte.score", data),
+            # "time-to-first-byte-potential-savings": PageSpeedHammer.property("lighthouseResult.audits.time-to-first-byte.details.overallSavingsMs", data),
+            # "render-blocking-resources-score": PageSpeedHammer.property("lighthouseResult.audits.render-blocking-resources.score", data),
+            # "render-blocking-resources-potential-savings": PageSpeedHammer.property("lighthouseResult.audits.render-blocking-resources.details.overallSavingsMs", data),
+            # "uses-text-compression-score": PageSpeedHammer.property("lighthouseResult.audits.uses-text-compression.score", data),
+            # "uses-text-compression-potential-savings": PageSpeedHammer.property("lighthouseResult.audits.uses-text-compression.details.overallSavingsMs", data),
+            # "bootup-time-score": PageSpeedHammer.property("lighthouseResult.audits.bootup-time.score", data),
+            # "bootup-time-wasted-time": PageSpeedHammer.property("lighthouseResult.audits.bootup-time.details.summary.wastedMs", data),
         }
         return result
     
